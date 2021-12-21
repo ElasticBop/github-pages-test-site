4026e0f24ea556c4f7c0877d1c0085295ce88028
ElasticBop <53546840+ElasticBop@users.noreply.github.com>
2021-12-21 11:28:55 -0800

    added animation with intersection observer

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+        // Add the animation class
+            entry.target.classList.add('fade-animation');
+        }
+    });
+});
+  
+observer.observe(document.querySelector('.welcome-intro'));
+observer.observe(document.querySelector('.projects-entry'));

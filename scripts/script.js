90976a2f509afc0f225be7e92dd9d802afb98671
ElasticBop <53546840+ElasticBop@users.noreply.github.com>
2021-12-23 12:08:51 -0800

    added a function to add projects
    does not work with intersection observer

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,42 @@
+var projects = [{
+    description : "Resume site built with raw HTML, CSS, and Javascript. This was my first attempt at building a website that includes responsive capabilities as well as animations. The project is WIP and will be expanded on in the future.",
+    link : "https://github.com/elasticbop",
+    img : "./projects/images/project1.jpg"
+}, {
+    description : "Second project",
+    link : "https://github.com/elasticbop",
+    img : "./projects/images/project1.jpg"        
+}];
+
+function addProject(project) {
+    var link = document.createElement("a");
+    var paragraph = document.createElement("p");
+    var img = document.createElement("img");
+    var figure = document.createElement("figure");
+    var figcaption = document.createElement("figcaption");
+    var container = document.createElement("div");
+
+    link.setAttribute("href", project.link);
+    link.setAttribute("target", "_blank");
+    link.setAttribute("class", "projects-entry-image-cover");
+    
+    paragraph.appendChild(document.createTextNode(project.description));
+
+    img.setAttribute("src", project.img);
+    img.setAttribute("alt", "No Image Available");
+
+    container.setAttribute("class", "projects-entry fade-animation");
+
+    img.appendChild(link);
+    figure.appendChild(img);
+
+    figcaption.appendChild(paragraph);
+    figure.appendChild(figcaption);
+
+    container.appendChild(figure);
+    document.getElementById("projects-container").appendChild(container);
+}
+
 function removeLoadingCover() {
     var cover = document.getElementById("loading-screen");
     var content = document.getElementById("content");
